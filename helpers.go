f357bf8613a7a93ba0e8d1b27d39939398678bff
doublenns <nnamdi.anyanwu@gmail.com>
2021-05-25 06:51:26 -0500

    Helper functions outside of library menu functions

--- helpers.go
+++ helpers.go
@@ -1,8 +1,40 @@
 package main
 
-import "github.com/inancgumus/screen"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/inancgumus/screen"
+)
 
 func clearScreen() {
 	screen.Clear()
 	screen.MoveTopLeft()
+}
+
+func readLibraryFromFile() {
+	byteContacts, err := os.ReadFile("library.json")
+	if os.IsNotExist(err) {
+		return
+	} else if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	err = json.Unmarshal(byteContacts, &library)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+}
+
+func saveToFile() {
+
+}
+
+func exitProgram() {
+	fmt.Println("Quitting Go Library...")
+	time.Sleep(2 * time.Second)
+	quit = true
+	os.Exit(0)
 }
