093b877cae2d03096ea48dfd8145c6f664a54227
doublenns <nnamdi.anyanwu@gmail.com>
2021-05-25 06:52:28 -0500

    Split menu out into it's own file again

--- functions.go
+++ functions.go
@@ -1,23 +1,5 @@
 package main
 
-import "fmt"
-
-var menuItems = map[int]option{
-	1: view,
-	2: add,
-	3: edit,
-	4: del,
-	5: search,
-	6: exit,
-}
-
-func printMenu() {
-	for i := 1; i < 7; i++ {
-		fmt.Printf("%d) %s\n", i, menuItems[i].description)
-	}
-	fmt.Print("\nChoose [1-6]: ")
-}
-
 func viewBooks() {
 
 }
