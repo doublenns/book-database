51861fbbcfe316a9a053180ebe03d322df580ccc
doublenns <nnamdi.anyanwu@gmail.com>
2021-05-26 08:19:45 -0500

    New fucntion to validate book selection

--- input.go
+++ input.go
@@ -27,3 +27,19 @@ func validateMenuSelection() (int, error) {
 	}
 	return 999, errors.New(errorMessage)
 }
+
+func validateBookSelection() (int, error) {
+	errorMessage := fmt.Sprintf("Invalid book selection -- selection wasn't a number from 1 - %v", len(library))
+
+	rawInput := getInput()
+	if rawInput == "" {
+		return 500, nil
+	}
+	input, err := strconv.Atoi(rawInput)
+	if err == nil {
+		if input >= 1 && input <= len(library) {
+			return input, nil
+		}
+	}
+	return 999, errors.New(errorMessage)
+}
